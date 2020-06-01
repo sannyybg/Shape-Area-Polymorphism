ddba3c9866ec6512fbc9ff2f16fd5d68df3f4f7e
sandro gagoshidze <49387424+sannyybg@users.noreply.github.com>
2020-06-01 10:29:03 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new Shapes();
+            Console.WriteLine(g.ShapesArea(6, 3, 2, 4));
+            Console.WriteLine(g.ShapesArea(3, 8, 1, 5, 6, -1));
+            Console.WriteLine(g.ShapesArea(-2, -3, -6, 4, 5, 3, 6, -1));
+
+            Console.ReadKey();
+
+
+
+            /* Shapes[] myshapes = new Shapes[3];
+            myshapes[0] = [2, 3, 5, 6];
+            myshapes[1] = [-5, 1, -2, 5, 6, -1];
+            myshapes[2] = [-4, -2, -1, 3, 6, 6, 4, -1];
+
+
+
+            for (int i = 0; i < myshapes.Length; i++)
+               {
+               
+                var sh = myshapes[i];
+                Console.WriteLine(sh.ShapesArea());
+               }
+
+            */
+
+        }   
+
+    }
+}
---
+++ Shapes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+   
+
+
+    public class Shapes
+    {
+
+        public double ShapesArea()
+        {
+            return 0;
+        }
+
+        public double ShapesArea(double a, double b, double c, double d)
+        {
+            var CircleRad = Math.Pow((Math.Pow((d - b), 2) + Math.Pow((c - a), 2)), 0.5);
+            return CircleRad * CircleRad * Math.PI;
+        }
+
+
+        public double ShapesArea(double a, double b, double c, double d, double e, double f)
+        {
+            return Math.Abs((a*(d-f) + c*(f-b) + e*(b-d)) / 2);  
+        }
+
+
+        public double ShapesArea(double a, double b, double c, double d, double e, double f, double g, double h)
+        {
+            return Math.Abs((a*(d-f) + c*(f-b) + e*(b-d)) / 2) + Math.Abs((e*(h-b) + g*(b-f) + a*(e-g)) / 2);
+        }
+
+
+    }
+
+    
+}
+
+    
